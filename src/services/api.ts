f043fbe401aef4c5509ac56c31ac461e7d1ccacf
Cristian Loaiza <kla-1997@hotmail.com>
2022-04-06 13:04:14 -0500

    master: Fix heroku address

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,7 @@ import { fast } from "../routes/router";
 
 const start = async () => {
   try {
-    console.log(process.env);
     // En heroku no se puede definir el puerto directamente y cambiar el localhost a 0.0.0.0
-    // await fast.listen(process.env.PORT || 5000, "localhost");
     await fast.listen(process.env.PORT || 5000, "0.0.0.0");
   } catch (error) {
     fast.log.error(error);
